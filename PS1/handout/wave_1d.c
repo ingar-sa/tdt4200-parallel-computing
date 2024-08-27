2f5a980fd08620ce761ec1b89e7b843101737125
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2024-08-27 19:38:18 +0200

    Started work on PS1

--- PS1/handout/wave_1d.c
+++ PS1/handout/wave_1d.c
@@ -1,98 +1,176 @@
 #define _XOPEN_SOURCE 600
+#include "isa.h"
 #include <math.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// Option to change numerical precision.
-typedef int64_t int_t;
-typedef double  real_t;
+ISA_LOG_REGISTER(wave_1d);
 
 // Simulation parameters: size, step count, and how often to save the state.
-const int_t N             = 1024;
-const int_t max_iteration = 4000;
-const int_t snapshot_freq = 10;
+static struct
+{
+    const i64 N;
+    const i64 MaxIterations;
+    const i64 SnapshotFrequency;
+} SimParams = { .N = 1024, .MaxIterations = 4000, .SnapshotFrequency = 10 };
 
 // Wave equation parameters, time step is derived from the space step.
-const real_t c = 1.0, dx = 1.0;
-real_t       dt;
+static struct
+{
+    const f64 c;
+    const f64 dx;
+    f64       dt;
+} WaveEquationParams = { .c = 1.0, .dx = 1.0 };
 
 // Buffers for three time steps, indexed with 2 ghost points for the boundary.
-real_t *buffers[3] = { NULL, NULL, NULL };
+// Each buffer contains all of the values for the entire wave at that time step
+static struct
+{
+    f64 *PrevStep;
+    f64 *CurrStep;
+    f64 *NextStep;
+} TimeSteps;
 
-#define U_prv(i) buffers[0][(i) + 1]
-#define U(i)     buffers[1][(i) + 1]
-#define U_nxt(i) buffers[2][(i) + 1]
+#define UPrev(i) TimeSteps.PrevStep[(i) + 1]
+#define UCurr(i) TimeSteps.CurrStep[(i) + 1]
+#define UNext(i) TimeSteps.NextStep[(i) + 1]
 
 // Save the present time step in a numbered file under 'data/'.
 void
-domain_save(int_t step)
+DomainSave(i64 Step)
 {
-    char filename[256];
-    sprintf(filename, "data/%.5ld.dat", step);
-    FILE *out = fopen(filename, "wb");
-    fwrite(&U(0), sizeof(real_t), N, out);
-    fclose(out);
+    char Filename[256];
+    sprintf(Filename, "data/%.5ld.dat", Step);
+    FILE *Out = fopen(Filename, "wb");
+    fwrite(&UCurr(0), sizeof(f64), SimParams.N, Out);
+    fclose(Out);
 }
 
 // TASK: T1
 // Set up our three buffers, fill two with an initial cosine wave,
 // and set the time step.
 void
-domain_initialize(void)
+DomainInitialize(isa_arena *Arena)
 {
-    // BEGIN: T1
-    ;
-    // END: T1
+    // +2 for the ghost points (???)
+    TimeSteps.PrevStep = IsaPushArray(Arena, f64, SimParams.N + 2);
+    TimeSteps.CurrStep = IsaPushArray(Arena, f64, SimParams.N + 2);
+    TimeSteps.NextStep = IsaPushArray(Arena, f64, SimParams.N + 2);
+
+    for(int i = 0; i < SimParams.N; ++i)
+    {
+        f64 Point = cos(M_PI * ((f64)i / (f64)SimParams.N));
+        UPrev(i)  = Point;
+        UCurr(i)  = Point;
+        // UNext(i) = Point; ???
+    }
+
+    WaveEquationParams.dt = WaveEquationParams.c / WaveEquationParams.dx; //???
 }
 
 // TASK T2:
 // Return the memory to the OS.
 // BEGIN: T2
 void
-domain_finalize(void)
+DomainFinalize(void)
 {
-    ;
-}
-// END: T2
+
+    /*
+     * Since I use an arena allocator, and its memory is
+     * alive for the entire program, this step is unnecessary.
+     * Explicitly freeing memory that will live for the entire
+     * program is also redundant since it will be reclaimed by the
+     * OS when the process exits regardless. If there are security
+     * concerns, then explicityly clearing the memory before the
+     * program is finished could be done, but the OS *should*
+     * do this as well.
+     */
+
+} // END: T2
 
 // TASK: T3
 // Rotate the time step buffers.
 // BEGIN: T3
-;
+void
+PerformTimeStep(void)
+{
+    // Simply rotate the pointers. No need to copy data.
+    f64 *PrevStep      = TimeSteps.PrevStep;
+    TimeSteps.PrevStep = TimeSteps.CurrStep;
+    TimeSteps.CurrStep = TimeSteps.NextStep;
+    TimeSteps.NextStep = PrevStep;
+}
 // END: T3
 
 // TASK: T4
 // Derive step t+1 from steps t and t-1.
 // BEGIN: T4
-;
+inline f64
+Integrate(i64 i)
+{
+    // Formula U(t+1, i) = -U(t-1, i) + 2*U(t, i) + [((dt^2 * c^2)/(h^2=dx^2) * (U(t, i-1) + U(t, i+1) - 2*U(t, i))]
+
+    f64 NewUNext;
+    f64 UCurrPrevPoint, UCurrThisPoint, UCurrNextPoint;
+    f64 UPrev;
+    f64 dt = WaveEquationParams.dt, c = WaveEquationParams.c, dx = WaveEquationParams.dx;
+
+    UCurrPrevPoint = UCurr(i - 1);
+    UCurrThisPoint = UCurr(i);
+    UCurrNextPoint = UCurr(i + 1);
+    UPrev          = UPrev(i);
+
+    NewUNext = -UPrev + 2 * UCurrThisPoint
+             + (((dt * dt * c * c) / (dx * dx)) * (UCurrPrevPoint + UCurrNextPoint - 2 * UCurrThisPoint));
+
+    return NewUNext;
+}
 // END: T4
 
 // TASK: T5
 // Neumann (reflective) boundary condition.
 // BEGIN: T5
-;
+void
+NeumannBoundary(void)
+{
+}
 // END: T5
 
 // TASK: T6
 // Main time integration.
 void
-simulate(void)
+Simulate(void)
 {
     // BEGIN: T6
-    int_t iteration = 0;
-    domain_save(iteration / snapshot_freq);
+    i64 Iteration = 0;
+    DomainSave(Iteration / SimParams.SnapshotFrequency);
+
+#if 0
+    {
+        printf("%f %s", NewUNext, (i % 10 == 0) ? "\n" : "");
+    }
+#endif
+
     // END: T6
 }
 
 int
 main(void)
 {
-    domain_initialize();
+    isa_arena Arena;
+    u64       ArenaSize = IsaKibiByte(64);
+    u8       *Mem       = malloc(ArenaSize);
+    if(NULL == Mem)
+    {
+        perror("Failed to allocate memory!");
+        exit(EXIT_FAILURE);
+    }
+    IsaArenaInit(&Arena, Mem, ArenaSize);
 
-    simulate();
+    DomainInitialize(&Arena);
+    Simulate();
+    // DomainFinalize();
 
-    domain_finalize();
     exit(EXIT_SUCCESS);
 }
