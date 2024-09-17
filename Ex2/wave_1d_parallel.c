7acec257d298145da58742a0b992f783f4897001
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2024-09-17 17:23:00 +0200

    Continued work on exercise 2

--- Ex2/wave_1d_parallel.c
+++ Ex2/wave_1d_parallel.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <sys/time.h>
 
@@ -19,7 +20,25 @@ typedef double  f64;
 // TASK: T1b
 // Declare variables each MPI process will need
 // BEGIN: T1b
-;
+typedef struct
+{
+    i64 MyRank;
+    i64 CommSize;
+
+    bool IAmRootRank;
+    bool IAmFirstRank;
+    bool IAmLastRank;
+
+    i64 CellsPerRank;
+    i64 RemainingCells;
+
+    i64 NMyCells;
+    // TODO(ingar): These two might not be needed
+    i64 StartingCell;
+    i64 EndingCell;
+} mpi_params;
+
+static mpi_params MpiParams = {};
 // END: T1b
 
 // Simulation parameters: size, step count, and how often to save the state.
@@ -62,14 +81,14 @@ static time_steps TimeSteps = {};
 
 // TASK: T8
 // Save the present time step in a numbered file under 'data/'.
-void
-SaveDomain(i64 step)
+static void
+SaveDomain(i64 Step)
 {
     // BEGIN: T8
     char filename[256];
-    sprintf(filename, "data/%.5ld.dat", step);
+    sprintf(filename, "data/%.5ld.dat", Step);
     FILE *out = fopen(filename, "wb");
-    fwrite(&UCurr(0), sizeof(f64), SimParams.NTimeSteps, out);
+    fwrite(&UCurr(0), sizeof(f64), SimParams.NCells, out);
     fclose(out);
     // END: T8
 }
@@ -78,19 +97,45 @@ SaveDomain(i64 step)
 // Allocate space for each process' sub-grids
 // Set up our three buffers, fill two with an initial cosine wave,
 // and set the time step.
-void
-InitializeDomain(int CommSize, int MyRank)
+static void
+InitializeDomain(void)
 {
     // BEGIN: T3
-    TimeSteps.PrevStep = malloc((SimParams.NCells + 2) * sizeof(f64));
-    TimeSteps.CurrStep = malloc((SimParams.NCells + 2) * sizeof(f64));
-    TimeSteps.NextStep = malloc((SimParams.NCells + 2) * sizeof(f64));
+    // TODO(ingar): Verify that giving the remainder of the cells to the final rank works/is correct
+    MpiParams.CellsPerRank   = (SimParams.NCells / MpiParams.CommSize);
+    MpiParams.RemainingCells = SimParams.NCells % MpiParams.CommSize;
+    if(MpiParams.IAmRootRank)
+    {
+        MpiParams.NMyCells = SimParams.NCells;
+        // TODO(ingar): Verify that this works / move root's buffer for itself?
+        TimeSteps.CurrStep = malloc((SimParams.NCells + 2) * sizeof(f64));
+        return;
+    }
+    else if(MpiParams.IAmLastRank)
+    {
+        MpiParams.NMyCells = MpiParams.CellsPerRank + MpiParams.RemainingCells;
+    }
+    else
+    {
+        MpiParams.NMyCells = MpiParams.CellsPerRank;
+    }
 
-    for(i64 i = 0; i < SimParams.NCells; i++)
+    TimeSteps.PrevStep = malloc((MpiParams.NMyCells + 2) * sizeof(f64));
+    TimeSteps.CurrStep = malloc((MpiParams.NMyCells + 2) * sizeof(f64));
+    TimeSteps.NextStep = malloc((MpiParams.NMyCells + 2) * sizeof(f64));
+
+    // TODO(ingar): This might only be usefule here
+    MpiParams.StartingCell = (MpiParams.MyRank - 1) * MpiParams.CellsPerRank;
+    MpiParams.EndingCell   = MpiParams.StartingCell + MpiParams.NMyCells;
+
+    // NOTE(ingar): We must offset i to the correct part of the entire domain for the initialization
+    // values to be correct
+    for(i64 i = MpiParams.StartingCell; i < MpiParams.EndingCell; ++i)
     {
         f64 Point = cos(M_PI * i / (f64)SimParams.NCells);
-        UPrev(i)  = Point;
-        UCurr(i)  = Point;
+
+        UPrev(i - MpiParams.StartingCell) = Point;
+        UCurr(i - MpiParams.StartingCell) = Point;
     }
     // END: T3
 
@@ -102,6 +147,7 @@ InitializeDomain(int CommSize, int MyRank)
 void
 domain_finalize(void)
 {
+    // TODO(ingar): Might not be unnecessary since every process allocates their own memory
     // Unnecessary. The memory is live for the entire program and will be reclaimed on process exit
     // free(buffers[0]);
     // free(buffers[1]);
@@ -112,6 +158,7 @@ domain_finalize(void)
 static inline void
 RotateBuffers(void)
 {
+    // TODO(ingar): WILL NOT WORK FOR THE ROOT!!!
     f64 *PrevStep      = TimeSteps.PrevStep;
     TimeSteps.PrevStep = TimeSteps.CurrStep;
     TimeSteps.CurrStep = TimeSteps.NextStep;
@@ -123,8 +170,11 @@ RotateBuffers(void)
 static inline void
 PerformTimeStep(void)
 {
-    f64 dt = WaveEquationParams.dt, c = WaveEquationParams.c, dx = WaveEquationParams.dx;
-    for(i64 i = 0; i < SimParams.NCells; ++i)
+    f64 dt = WaveEquationParams.dt;
+    f64 c  = WaveEquationParams.c;
+    f64 dx = WaveEquationParams.dx;
+
+    for(i64 i = 0; i < MpiParams.NMyCells; ++i)
     {
         UNext(i) = -UPrev(i) + 2.0 * UCurr(i)
                  + (dt * dt * c * c) / (dx * dx) * (UCurr(i - 1) + UCurr(i + 1) - 2.0 * UCurr(i));
@@ -148,7 +198,35 @@ void
 PerformBorderExchange(void)
 {
     // BEGIN: T5
-    ;
+
+    if(MpiParams.IAmFirstRank)
+    {
+        f64 *BorderSend = TimeSteps.CurrStep + MpiParams.NMyCells;
+        MPI_Send(BorderSend, 1, MPI_DOUBLE, MpiParams.MyRank + 1, 0, MPI_COMM_WORLD);
+    }
+    else if(MpiParams.IAmLastRank)
+    {
+        f64 *BorderRecv = TimeSteps.CurrStep;
+        MPI_Recv(BorderRecv, 1, MPI_DOUBLE, MpiParams.MyRank - 1, 0, MPI_COMM_WORLD,
+                 MPI_STATUS_IGNORE);
+    }
+    else
+    {
+        f64 *LeftBorderSend = &UCurr(0);
+        MPI_Send(LeftBorderSend, 1, MPI_DOUBLE, MpiParams.MyRank + 1, 0, MPI_COMM_WORLD);
+
+        f64 *LeftBorderRecv = &UCurr(-1);
+        MPI_Recv(LeftBorderRecv, 1, MPI_DOUBLE, MpiParams.MyRank - 1, 0, MPI_COMM_WORLD,
+                 MPI_STATUS_IGNORE);
+
+        f64 *RightBorderSend = &UCurr(MpiParams.NMyCells - 1);
+        MPI_Send(RightBorderSend, 1, MPI_DOUBLE, MpiParams.MyRank + 1, 0, MPI_COMM_WORLD);
+
+        f64 *RightBorderRecv = &UCurr(MpiParams.NMyCells);
+        MPI_Recv(RightBorderRecv, 1, MPI_DOUBLE, MpiParams.MyRank - 1, 0, MPI_COMM_WORLD,
+                 MPI_STATUS_IGNORE);
+    }
+
     // END: T5
 }
 
@@ -168,7 +246,7 @@ void
 Simulate(void)
 {
     // Go through each time step.
-    for(i64 Iteration = 0; Iteration <= SimParams.NTimeSteps; Iteration++)
+    for(i64 Iteration = 0; Iteration <= SimParams.NTimeSteps; ++Iteration)
     {
         if(0 == (Iteration % SimParams.SnapshotFrequency))
         {
@@ -197,6 +275,19 @@ main(int ArgCount, char **ArgV)
     MPI_Comm_size(MPI_COMM_WORLD, &CommSize);
     MPI_Comm_rank(MPI_COMM_WORLD, &MyRank);
 
+    if(CommSize > SimParams.NCells)
+    {
+        printf("Cannot use more processes than simulation cells!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // TODO(ingar): Verify that this is correct
+    MpiParams.MyRank       = MyRank;
+    MpiParams.CommSize     = CommSize;
+    MpiParams.IAmRootRank  = MyRank == 0;
+    MpiParams.IAmFirstRank = MyRank == 1;
+    MpiParams.IAmLastRank  = MyRank == (CommSize - 1);
+
     // END: T1c
 
     // TODO(ingar): Figure out why they included this instead of doubles, which is what is used by
@@ -205,7 +296,7 @@ main(int ArgCount, char **ArgV)
     f64 TimeStart = 0.0;
     f64 TimeEnd   = 0.0;
 
-    InitializeDomain(CommSize, MyRank);
+    InitializeDomain();
 
     // TASK: T2
     // Time your code
@@ -214,11 +305,19 @@ main(int ArgCount, char **ArgV)
     // TODO(ingar): Only rank 0 should perform the timing, since we're interested in the time the
     // entire simulation takes, not how long each process uses for its slice of it. I don't think we
     // need to synchronize the ranks by using barriers in this case, but I'm unsure
-    TimeStart = MPI_Wtime();
+    if(MpiParams.IAmRootRank)
+    {
+        TimeStart = MPI_Wtime();
+    }
+    else
+    {
+        Simulate();
+    }
 
-    Simulate();
-
-    TimeEnd = MPI_Wtime();
+    if(MpiParams.IAmRootRank)
+    {
+        TimeEnd = MPI_Wtime();
+    }
     // END: T2
 
     // domain_finalize();
